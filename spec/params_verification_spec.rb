1a428a0c3624913182767dfe7179fc828dccf7ff
Maksim Horbul <max@gorbul.net>
2011-11-01 17:58:42 -0700

    remove whitespaces

--- spec/params_verification_spec.rb
+++ spec/params_verification_spec.rb
@@ -1,25 +1,32 @@
 require_relative "spec_helper"
 
 describe ParamsVerification do
-  
+
   before :all do
     @service = WSList.all.find{|s| s.url == 'services/test.xml'}
     @service.should_not be_nil
     @valid_params = {'framework' => 'RSpec', 'version' => '1.02', 'user' => {'id' => '123'}}
   end
-  
+
   it "should validate valid params" do
     params = @valid_params.dup
     lambda{ ParamsVerification.validate!(params, @service.defined_params) }.should_not raise_exception
   end
-  
+
   it "should return the params" do
     params = @valid_params.dup
     returned_params = ParamsVerification.validate!(params, @service.defined_params)
     returned_params.should be_an_instance_of(Hash)
     returned_params.keys.size.should >= 3
   end
-  
+
+  it "should return the params" do
+    params = @valid_params.dup
+    returned_params = ParamsVerification.validate!(params, @service.defined_params)
+    returned_params.should be_an_instance_of(Hash)
+    returned_params.keys.size.should >= 3
+  end
+
   it "should set the default value for an optional param" do
     params = @valid_params.dup
     params[:timestamp].should be_nil
@@ -33,7 +40,7 @@ describe ParamsVerification do
     returned_params = ParamsVerification.validate!(params, @service.defined_params)
     returned_params[:user][:mailing_list].should be_true
   end
-  
+
   it "should raise an exception when a required param is missing" do
     params = @valid_params.dup
     params.delete('framework')
@@ -53,19 +60,19 @@ describe ParamsVerification do
     params = {'seq' => "a b c d e g"}
     lambda{ ParamsVerification.validate!(params, service.defined_params) }.should_not raise_exception(ParamsVerification::InvalidParamType)
   end
-  
+
   it "should raise an exception when a param is of the wrong type" do
     params = @valid_params.dup
     params['user']['id'] = 'abc'
     lambda{ ParamsVerification.validate!(params, @service.defined_params) }.should raise_exception(ParamsVerification::InvalidParamType)
   end
-  
+
   it "should raise an exception when a param is under the minvalue" do
     params = @valid_params.dup
     params['num'] = 1
     lambda{ ParamsVerification.validate!(params, @service.defined_params) }.should raise_exception(ParamsVerification::InvalidParamValue)
   end
-  
+
   it "should raise an exception when a param isn't in the param option list" do
     params = @valid_params.dup
     params['alpha'] = 'z'
@@ -82,12 +89,12 @@ describe ParamsVerification do
     service.should_not be_nil
     lambda{ ParamsVerification.validate!(params, service.defined_params) }.should raise_exception(ParamsVerification::InvalidParamValue)
   end
-  
+
   it "should validate that no params are passed when accept_no_params! is set on a service" do
     service = WSList.all.find{|s| s.url == "services/test_no_params.xml"}
     service.should_not be_nil
     params = @valid_params.dup
     lambda{ ParamsVerification.validate!(params, service.defined_params) }.should raise_exception
   end
-  
+
 end
